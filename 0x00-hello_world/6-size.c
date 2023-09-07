65966659a326f22f4686359cde2ad1a0d472a855
SopyEx <nplat46@gmail.com>
2023-09-07 14:03:20 -0700

    sc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - A programe that prints the size of various computer types
- * Return 0
+ * *Description: main - A programe that prints the size of various computer types
+ * *Return 0
  */
 int main(void)
 {
