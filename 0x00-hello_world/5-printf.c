a0da4fb6d92ce9c9268f9fc84298a293b9874663
SopyEx <nplat46@gmail.com>
2023-09-07 13:53:54 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - A programe that prints a line in using the printf function
+ * *Description: main - A programe that prints a line in using the printf function
  * Return: 0
  */
 
