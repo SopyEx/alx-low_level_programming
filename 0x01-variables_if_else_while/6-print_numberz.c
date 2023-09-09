a6ad870b16b83d8f8eb0eb46d77b8cb4124a4358
SopyEx <nplat46@gmail.com>
2023-09-09 10:54:58 -0700

    6print

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar((i % 10) + '0');
+	printf("\n");
+	return (0);
+}
