5fe837da50bdce67923eebafaf299ba1d7461204
SopyEx <nplat46@gmail.com>
2023-09-13 16:08:11 -0700

    time

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,21 +1,34 @@
 #include "main.h"
 /**
- * main - main function
- * Return: 0 always
+ * times_table - main function
+ * Example Table
+ * 0, 0, 0,...
+ * 0, 1, 2,...
  */
 
 void times_table(void)
 {
-	int i, j;
+	int num, nult, prod;
 
-	for (i = 0 ; i < 100 ; i++)
+	for (num = 0; num <= 9; num++)
 	{
-		for (j = 0 ; j < 10 ; j++)
+		_putchar(48);
+		for (nult = 1; nult <= 9; nult++)
 		{
-			result = i * j;
-			printf("%d", result);
+			_putchar('.');
+			_putchar(' ');
+
+			prod = num * nult;
+			/*
+			 * put space if product is asingle number
+			 * place the first digit if its two numbers
+			 */
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + 48); /*get the fist digit*/
+			_putchar((prod % 10) + 48); /*get the second digit*/
 		}
+		_putchar('\n');
 	}
-	printf('\n');
 }
-	
