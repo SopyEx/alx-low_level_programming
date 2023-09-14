b29a099b4315f6b7f76d4fe79c370a535471fb4c
SopyEx <nplat46@gmail.com>
2023-09-13 17:33:00 -0700

    natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * main - Entry point
+ * Return: 0 always
+ */
+int main(void)
+{
+	int sum, num;
+
+	for (num = 0; num < 1024; num++)
+	{
+		if ((num % 3 == 0) || (num % 5 == 0))
+			sum = num;
+	}
+	printf("%d\n", sum);
+
+	return (0);
+}
