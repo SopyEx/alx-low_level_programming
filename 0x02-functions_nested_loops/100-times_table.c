4ca231e86894b64aaa6cb64df5001a32c4f5f267
SopyEx <nplat46@gmail.com>
2023-09-13 17:21:58 -0700

    table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include"main.h"
+
+/**
+ * print_times_table - main function
+ * @n: takes input number
+ */
+void print_times_table(int n)
+{
+	int prod, nult, num;
+
+	if (n <= 15 && n >= 0)
+	{
+		for (num = 0; num <= n; ++num)
+		{
+			_putchar(48);
+			for (nult = 1; nult <= n; ++nult)
+			{
+				_putchar(',');
+				_putchar(' ');
+
+				prod = num * nult;
+
+				if (prod <= 9)
+					_putchar(' ');
+				if (prod <= 99)
+					_putchar(' ');
+
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				}
+				else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			_putchar('\n');
+		}
+	}
+}
