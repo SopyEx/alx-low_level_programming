e98d6eb74eedf4079d034d59c80691c7c14efc9d
SopyEx <nplat46@gmail.com>
2023-09-15 06:43:01 -0700

    done

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,4 +1,5 @@
 #include"main.h"
+#include<stdio.h>
 /**
  * print_line - main function
  * @n: number of time printed
@@ -6,13 +7,13 @@
 
 void print_line(int n)
 {
-	int 1nchr;
+	int 1nChr;
 
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		for (1nchr = 1; 1nchr <= n; 1nchr++)
+		for (1nChr = 1; 1nChr <= n; 1nChr++)
 			_putchar('_');
 		_putchar('\n');
 	}
