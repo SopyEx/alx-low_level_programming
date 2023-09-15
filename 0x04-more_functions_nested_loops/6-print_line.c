0fe47657864d82a72e749a98e6390d984440006a
SopyEx <nplat46@gmail.com>
2023-09-15 06:32:20 -0700

    line 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include"main.h"
+/**
+ * print_line - main function
+ * @n: number of time printed
+ */
+
+void print_line(int n)
+{
+	int inchr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (inchr = 1; inchr <= n inchr++
+			_putchar('_');
+		_putchar('\n');
+	}
+}
