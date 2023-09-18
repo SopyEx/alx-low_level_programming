0d2e3d9c14d115fcf1ce9e5a4170bfb8f9fce776
SopyEx <nplat46@gmail.com>
2023-09-18 09:46:54 -0700

    arry

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include"main.h"
+/**
+ * print_array - main function
+ * @a: arry to be used
+ * @n: number of element to be printed
+ * Return: void
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i < n - 1)
+			printf(",");
+	}
+	printf("\n");
+}
