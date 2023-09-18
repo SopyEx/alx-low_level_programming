e3331a7bfd56d91f78e87bcb29f54be0c74f344b
SopyEx <nplat46@gmail.com>
2023-09-18 07:57:11 -0700

    puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,14 +1,14 @@
 #include"main.h"
 /**
  * _puts - main function
- * @str: strings
- * Return: nothing
+ * @str: strings to be printed
+ * Return: void
  */
 
 void _puts(char *str)
 {
 	int i;
 
-	for(i = 0 ; str[i] != '\0' : i++)
+	for(i = 0; str[i] != '\0'; i++)
 		_putchar(str[i]);
-}
+
