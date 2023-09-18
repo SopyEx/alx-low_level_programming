633cf8c1822cdc5cd2f2cc6a5c270f8fbd8644fd
SopyEx <nplat46@gmail.com>
2023-09-18 09:09:41 -0700

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,7 +8,7 @@ void puts_half(char *str)
 {
 	int i, n, count = 0;
 
-	for (i = o; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 		count++;
 	n = (count - 1) / 2;
 	for (i = n + 1; str[i] != '\0'; i++)
