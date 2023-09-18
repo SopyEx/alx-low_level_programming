66c44c1038c12a33e3f3d0232f5a139257ed0bdc
SopyEx <nplat46@gmail.com>
2023-09-18 07:37:06 -0700

    len

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+ *_strlen - main function
+ * @s: string
+ * Return: lenght of string
+ */
+
+int _strlen(char *s)
+{
+	int counter;
+
+	for (counter = 0; *s != '\0'; s++)
+		++counter;
+	return (counter);
+}
