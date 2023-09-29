38acaf97c4b727914a7cf87c0887c1f74f1ce573
SopyEx <nplat46@gmail.com>
2023-09-29 08:12:02 -0700

    prim

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,9 +1,12 @@
-#include"main.h"
+#include "main.h"
+
 int actual_prime(int n, int i);
+
 /**
- * is_prime_number - main fnx
- * @n: numbrer to evaluate
- * Return: 1 if n is a prime number 0 if not
+ * is_prime_number - says if an integer is a prime number or not
+ * @n: number to evaluate
+ *
+ * Return: 1 if n is a prime number, 0 if not
  */
 int is_prime_number(int n)
 {
@@ -13,16 +16,17 @@ int is_prime_number(int n)
 }
 
 /**
- * actual_prime - cal if number is a prime recursively
- * @n: number to eveluate
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
  * @i: iterator
- * Return: 1 if n is a prime 0 if not
+ *
+ * Return: 1 if n is prime, 0 if not
  */
 int actual_prime(int n, int i)
 {
 	if (i == 1)
 		return (1);
-	if (n % 1 == 0 && 1 > 0)
+	if (n % i == 0 && i > 0)
 		return (0);
 	return (actual_prime(n, i - 1));
 }
